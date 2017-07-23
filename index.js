d8a01f04b921878bfeed16a688eb5638b4898523
Michael Theriot <michael.lee.theriot@gmail.com>
2017-07-23 00:34:44 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const RCONConnection = require('./lib/rconconnection');
+const RCONError = require('./lib/rconerror');
+
+exports.RCONConnection = RCONConnection;
+exports.RCONError = RCONError;
